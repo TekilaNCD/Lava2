203551bf3136c10eb77c08aad7657d372ef521ce
Julian-git27 <julian.ramirez2727@gmail.com>
2025-08-31 22:18:13 -0500

    Primer montaje feactures/all

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -12,6 +12,4 @@ urlpatterns = [
     # Rutas para el login y el logout, usando las vistas que Django ya tiene
     path('login/', auth_views.LoginView.as_view(template_name='usuarios/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='usuarios/logout.html'), name='logout'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='usuarios/logout.html'), name='looogout'),
-    
 ]
